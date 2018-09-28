46689bc0bba9fa827efbb0546af6fa2eb1720be6
valentinech <valentinech@yandex.ru>
2018-09-28 15:49:46 +0400

    Add project files.

---
+++ DriverCleaner/Helper/ZipHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Xml.Linq;
+
+
+namespace DriverCleaner.Helper
+{
+    public static class ZipHelper
+    {
+        public static void ExtractFileToDirectory(string zipFileName, string outputDirectory)
+        {
+            if (Directory.Exists(outputDirectory))
+            {
+                var di = new DirectoryInfo(outputDirectory);
+                di.Attributes &= ~FileAttributes.ReadOnly;
+
+                foreach (var file in di.GetFiles("*", SearchOption.AllDirectories))
+                    file.Attributes &= ~FileAttributes.ReadOnly;
+
+                Directory.Delete(outputDirectory, true);
+            }
+
+            Directory.CreateDirectory(outputDirectory);
+
+            string zPath = "7za.exe"; //add to proj and set CopyToOuputDir
+            try
+            {
+                ProcessStartInfo pro = new ProcessStartInfo();
+                pro.WindowStyle = ProcessWindowStyle.Hidden;
+                pro.FileName = zPath;
+                pro.Arguments = string.Format("x \"{0}\" -y -o\"{1}\"", zipFileName, outputDirectory);
+                Process x = Process.Start(pro);
+                x.WaitForExit();
+            }
+            catch (System.Exception Ex)
+            {
+                //handle error
+            }
+
+            var directories = Directory.GetDirectories(outputDirectory);
+            foreach (var dir in directories)
+            {
+                var dirName = new DirectoryInfo(dir).Name;
+                if (!Properties.Settings.Default.directories.Contains(dirName))
+                {
+                    var di = new DirectoryInfo(dir);
+                    di.Attributes &= ~FileAttributes.ReadOnly;
+                    foreach (var file in di.GetFiles("*", SearchOption.AllDirectories))
+                        file.Attributes &= ~FileAttributes.ReadOnly;
+
+                    Directory.Delete(dir, true);
+                }
+            }
+
+           
+        }
+
+        public static void CleanCfg(string fileNameCfg)
+        {
+            XElement cfgXml = XElement.Load(fileNameCfg);
+            var elems = cfgXml.Element("manifest")?.Elements()
+                .Where(p => Properties.Settings.Default.eulaStr.Contains(p.Attribute("name")?.Value));
+            elems?.Remove();
+
+            cfgXml.Save(fileNameCfg);
+
+
+        }
+    }
+}
---
+++ DriverCleaner/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DriverCleaner.Helper;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+namespace DriverCleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string driverDirFullName = string.Empty;
+        private string zipFileName = string.Empty;
+        private string setupCfg = "setup.cfg";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new OpenFileDialog();
+            openDlg.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+
+            if (openDlg.ShowDialog() == true)
+            {
+                zipFileName = openDlg.FileName;
+                edtFileName.Text = openDlg.FileName;
+                driverDirFullName = Path.Combine(Path.GetDirectoryName(openDlg.FileName),
+                                        Path.GetFileNameWithoutExtension(openDlg.FileName));
+                edtLog.AppendText("Директория:\n");
+                edtLog.AppendText(driverDirFullName +"\n");
+                setupCfg = driverDirFullName + "\\" + setupCfg;
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            edtLog.AppendText("Распаковка\n");
+            ZipHelper.ExtractFileToDirectory(zipFileName, driverDirFullName);
+            edtLog.AppendText("Корректировка Setup.cfg\n");
+            ZipHelper.CleanCfg(setupCfg);
+        }
+    }
+}
